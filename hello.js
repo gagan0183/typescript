67fe1dd2e8ef2a2274fbeaf2b378bf726abd7e88
Gagan <g.deepsingh1@gmail.com>
2020-11-30 16:14:12 +0530

    enum typescript

--- hello.js
+++ hello.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log('hello');
+var State;
+(function (State) {
+    State[State["open"] = 0] = "open";
+    State[State["closed"] = 1] = "closed";
+    State[State["ajar"] = 2] = "ajar";
+})(State || (State = {}));
+console.log('enum string', State[State.closed]);
+console.log('enum value', State.closed);
+var StateString;
+(function (StateString) {
+    StateString["open"] = "open";
+    StateString["closed"] = "closed";
+    StateString["ajar"] = "ajar";
+})(StateString || (StateString = {}));
+console.log('enum state string', StateString[StateString.open]);
+console.log('enunb state value', StateString.open);
 //# sourceMappingURL=hello.js.map
--- hello.ts
+++ hello.ts
@@ -1,1 +1,18 @@
-console.log('hello');
+enum State {
+    open,
+    closed,
+    ajar
+}
+
+console.log('enum string', State[State.closed]);
+console.log('enum value', State.closed);
+
+enum StateString {
+    open = "open",
+    closed = "closed",
+    ajar = "ajar"
+}
+
+// ths syntax does not work with string
+console.log('enum state string', StateString[StateString.open]);
+console.log('enunb state value', StateString.open);
