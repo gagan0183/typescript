c3ac02f7638e9198763b1be2834a02d6561ee139
Gagan <g.deepsingh1@gmail.com>
2021-01-25 19:54:15 +0530

    enum

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
-var number1 = 5;
-var number2 = 5;
-var result = add(number1, number2);
-console.log(result);
+// const person: {
+//     name: string;
+//     canada: [number, string]
+// } = {
+//     name: 'Gagan',
+//     canada: [1, 'EDMONTON']
+// };
+// person.canada = [1, 'EDMONTON', ''];
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["EDITOR"] = 1] = "EDITOR";
+    Role[Role["VIEWER"] = 2] = "VIEWER";
+})(Role || (Role = {}));
+console.log(Role.ADMIN);
--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-const person: {
-    name: string;
-    canada: [number, string]
-} = {
-    name: 'Gagan',
-    canada: [1, 'EDMONTON']
-};
+// const person: {
+//     name: string;
+//     canada: [number, string]
+// } = {
+//     name: 'Gagan',
+//     canada: [1, 'EDMONTON']
+// };
 
-person.canada = [1, 'EDMONTON', ''];
+// person.canada = [1, 'EDMONTON', ''];
+
+enum Role { ADMIN, EDITOR, VIEWER}
+
+console.log(Role.ADMIN)
