a9e21c8b96c820e77b840b52586955e412fdd301
Gagan <g.deepsingh1@gmail.com>
2021-01-25 19:33:52 +0530

    tuples

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-function add(num1: number, num2: number) {
-    return num1 + num2;
-}
+const person: {
+    name: string;
+    canada: [number, string]
+} = {
+    name: 'Gagan',
+    canada: [1, 'EDMONTON']
+};
 
-const number1: number = 5;
-const number2: number = 5;
-
-const result = add(number1, number2);
-console.log(result);
+person.canada = [1, 'EDMONTON', ''];
