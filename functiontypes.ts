ebcc50884ce283c05b3fa64f13f79684c340ec4e
Gagan <g.deepsingh1@gmail.com>
2021-01-27 19:59:31 +0530

    types function

---
+++ functiontypes.ts
@@ -0,0 +1,7 @@
+const combine = (number1: number, number2: number): number => {
+    return number1 + number2;
+}
+
+let combineType: (number1: number, number2: number) => number;
+
+combineType = combine;
